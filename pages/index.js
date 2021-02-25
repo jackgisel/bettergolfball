b9d3cb02760ba6e76b19feb486fae8db12a813cd
Jack Gisel <jackrgisel@gmail.com>
2021-02-25 09:15:47 -0800

    update home page

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,22 @@ import Button from "../components/Button";
 
 const Home = () => {
   return (
-    <div className="bg-gray-100 h-screen w-screen flex  flex-col">
-      <div className="flex-1 flex justify-center items-center flex-col">
+    <div className="bg-gray-100 h-screen w-screen flex  flex-col items-center">
+      <div className="flex-1 flex justify-center  flex-col w-2/5 items-center">
         <img
-          className="h-15 rounded"
-          src="https://cdn.discordapp.com/attachments/766719205654921226/814366655018303498/logo_size_invert.jpg"
+          className="h-36"
+          src="https://cdn.discordapp.com/attachments/766719205654921226/814545979650867290/Black_and_Green_Framed_Fitness_Logo.png"
         />
-        <h2 className="text-2xl py-4 font-bold">
+        <h2 className="text-4xl py-4 font-bold">
           Welcome to Better Golf Balls
         </h2>
+        <p className="text-center pb-6">
+          Ever wanted to find your perfect golf ball, but don't know where to
+          start? Our simple 1 minute quiz will guide you to the right spot
+        </p>
         <Button title="Help me find my balls" />
       </div>
-      <div className="flex justify-center">
+      <div className="flex justify-center pb-3">
         <h3>BetterGolfBalls &copy; 2021</h3>
       </div>
     </div>
